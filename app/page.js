ea2e8885482594bce7d1faac2f6857f79a1cf0e0
EraKin575 <tejaskumar574@gmail.com>
2023-06-12 00:03:04 +0530

    land

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Discover And Share fghjk
+      <h1>Discover And Share fghjk fghjkl;
       <br className='max-md:hidden'>
         <span className='text-center'>AI Powered Prompts</span>
         
